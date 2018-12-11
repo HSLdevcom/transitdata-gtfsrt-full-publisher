3aff134de157d6af99cf4fdabf922549bde601c5
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-12-11 13:11:35 +0200

    Cleaned up azure uploading code

--- src/main/java/fi/hsl/transitdata/gtfsbundler/AzureSink.java
+++ src/main/java/fi/hsl/transitdata/gtfsbundler/AzureSink.java
@@ -12,10 +12,6 @@ import java.io.*;
 
 public class AzureSink implements ISink {
     private static final Logger log = LoggerFactory.getLogger(AzureSink.class);
-    //do this first: https://docs.microsoft.com/en-us/azure/storage/common/storage-quickstart-create-account?tabs=portal
-
-    //TODO https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-java
-    // https://github.com/Azure-Samples/storage-blobs-java-quickstart/blob/master/blobAzureApp/src/main/java/blobQuickstart/blobAzureApp/AzureApp.java
 
     private final String accountName;
     private final String accountKey;
@@ -28,7 +24,6 @@ public class AzureSink implements ISink {
         containerName = config.getString("bundler.output.azure.containerName");
     }
 
-
     @Override
     public void put(String name, byte[] data) throws Exception {
         upload(name, data, containerName, accountName, accountKey);
@@ -36,7 +31,7 @@ public class AzureSink implements ISink {
 
     private static void upload(String name, byte[] data, String containerName, String accountName, String accountKey) {
         log.info("Uploading file {} with {} kB to Azure Blob storage", name, (data.length / 1024));
-
+        final long startTime = System.currentTimeMillis();
         final String storageConnectionString = (new StringBuilder())
                 .append("DefaultEndpointsProtocol=https;")
                 .append("AccountName=").append(accountName).append(";")
@@ -46,77 +41,44 @@ public class AzureSink implements ISink {
         try {
             // Parse the connection string and create a blob client to interact with Blob storage
             final CloudStorageAccount storageAccount = CloudStorageAccount.parse(storageConnectionString);
+            log.debug("Got reference to StorageAccount");
             final CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+            log.debug("Got reference to BlobClient");
             final CloudBlobContainer container = blobClient.getContainerReference(containerName);
+            log.debug("Got reference to CloudBlobContainer");
 
             // Create the container if it does not exist with public access.
-            log.info("Creating container: " + container.getName());
-            container.createIfNotExists(BlobContainerPublicAccessType.CONTAINER, new BlobRequestOptions(), new OperationContext());
-            //TODO create snapshot? Or upload as compressed unique filename along with other.
-            //Creating a sample file
-            /*sourceFile = File.createTempFile("sampleFile", ".txt");
-            System.out.println("Creating a sample file at: " + sourceFile.toString());
-            BufferedWriter output = new BufferedWriter(new FileWriter(sourceFile));
-            output.write("Hello Azure!");
-            output.close();*/
+            boolean created = container.createIfNotExists(BlobContainerPublicAccessType.CONTAINER, new BlobRequestOptions(), new OperationContext());
+            if (created) {
+                log.warn("New container named {} created because existing wasn't found", container.getName());
+            }
 
-            //Getting a blob reference
             CloudBlockBlob blob = container.getBlockBlobReference(name);
+            log.debug("Got reference to CloudBlockBlob with name {}", blob.getName());
             final InputStream inputStream = new ByteArrayInputStream(data);
             final int length = data.length;
 
             //Creating blob and uploading file to it
-            System.out.println("Uploading the sample file ");
+            log.debug("Uploading the file ");
             blob.upload(inputStream, length);
-            //blob.uploadFromFile(sourceFile.getAbsolutePath());
 
             //Listing contents of container
+            log.info("Container contains files: ");
             for (ListBlobItem blobItem : container.listBlobs()) {
-                System.out.println("URI of blob is: " + blobItem.getUri());
+                log.info(blobItem.getUri().toString());
             }
 
-            // Download blob. In most cases, you would have to retrieve the reference
-            // to cloudBlockBlob here. However, we created that reference earlier, and
-            // haven't changed the blob we're interested in, so we can reuse it.
-            // Here we are creating a new file to download to. Alternatively you can also pass in the path as a string into downloadToFile method: blob.downloadToFile("/path/to/new/file").
-            //downloadedFile = new File(sourceFile.getParentFile(), "downloadedFile.txt");
-            //blob.downloadToFile(downloadedFile.getAbsolutePath());
         }
-        catch (StorageException ex)
-        {
+        catch (StorageException ex) {
             log.error("Error returned from the service. Http code: {} and error code: {}", ex.getHttpStatusCode(), ex.getErrorCode());
+            log.warn("Full stack trace", ex);
         }
-        catch (Exception ex)
-        {
-            log.error("Exception while uploading file to storage", ex);
+        catch (Exception ex) {
+            log.error("Unknown exception while uploading file to storage", ex);
         }
-        finally
-        {
-
-            /*
-            //Pausing for input
-            Scanner sc = new Scanner(System.in);
-            sc.nextLine();
-
-            System.out.println("Deleting the container");
-            try {
-                if(container != null)
-                    container.deleteIfExists();
-            }
-            catch (StorageException ex) {
-                System.out.println(String.format("Service error. Http code: %d and error code: %s", ex.getHttpStatusCode(), ex.getErrorCode()));
-            }
-
-            System.out.println("Deleting the source, and downloaded files");
-
-            if(downloadedFile != null)
-                downloadedFile.deleteOnExit();
-
-            if(sourceFile != null)
-                sourceFile.deleteOnExit();
-
-            //Closing scanner
-            sc.close();*/
+        finally {
+            long now = System.currentTimeMillis();
+            log.info("Upload finished in {} ms", (now - startTime));
         }
     }
 
