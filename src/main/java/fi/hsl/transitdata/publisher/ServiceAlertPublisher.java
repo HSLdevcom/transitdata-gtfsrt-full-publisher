014691cf7ac6d3971e8570c3b1becfb1ff4bd3dc
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-08-27 16:09:23 +0300

    Publish cancellation trip updates in service alert feed

--- src/main/java/fi/hsl/transitdata/publisher/ServiceAlertPublisher.java
+++ src/main/java/fi/hsl/transitdata/publisher/ServiceAlertPublisher.java
@@ -2,34 +2,98 @@ package fi.hsl.transitdata.publisher;
 
 import com.google.transit.realtime.GtfsRealtime;
 import com.typesafe.config.Config;
+import fi.hsl.common.gtfsrt.FeedMessageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class ServiceAlertPublisher extends DatasetPublisher {
     private static final Logger log = LoggerFactory.getLogger(DatasetPublisher.class);
 
+    private final long tripUpdateMaxAgeSecs;
+
+    private Map<String, GtfsRealtime.FeedEntity> tripUpdateCache = new HashMap<>();
+
+    private List<GtfsRealtime.FeedEntity> latestServiceAlerts;
+
     public ServiceAlertPublisher(Config config, ISink sink) {
         super(config, sink);
+
+        tripUpdateMaxAgeSecs = config.getDuration("bundler.tripUpdate.contentMaxAge", TimeUnit.SECONDS);
     }
 
     @Override
     public void publish(List<DatasetEntry> newMessages) throws Exception {
-        //We're only interested in the first item in the list.
-        Optional<DatasetEntry> latest = newMessages.stream()
-                .sorted(Comparator.comparingLong(DatasetEntry::getEventTimeUtcMs).reversed()) // Sort by event time, latest first
-                .findFirst();
-        if (latest.isPresent()) {
-            GtfsRealtime.FeedMessage msg = latest.get().getFeedMessage();
+        //Feed messages that contain textual service alerts
+        List<DatasetEntry> serviceAlerts = new ArrayList<>();
 
-            log.info("Publishing a new Service Alert");
-            byte[] data = msg.toByteArray();
-            sink.put(fileName, data);
+        for (DatasetEntry datasetEntry : newMessages) {
+            if (datasetEntry.getEntities().stream().allMatch(GtfsRealtime.FeedEntity::hasTripUpdate)) {
+                datasetEntry.getEntities().stream()
+                        .filter(feedEntity -> isCancellation(feedEntity.getTripUpdate()))
+                        .forEach(feedEntity -> {
+                            tripUpdateCache.put(feedEntity.getId(), feedEntity);
+                        });
+            } else if (datasetEntry.getEntities().stream().anyMatch(GtfsRealtime.FeedEntity::hasAlert)) {
+                serviceAlerts.add(datasetEntry);
+            } else {
+                log.warn("Unexpected feed message");
+            }
         }
+
+        //We're only interested in the first item in the list.
+        // Sort by event time, latest first
+        serviceAlerts.stream()
+                .max(Comparator.comparingLong(DatasetEntry::getEventTimeUtcMs))
+                .ifPresent(datasetEntry -> latestServiceAlerts = datasetEntry.getEntities());
+
+        long nowSecs = System.currentTimeMillis() / 1000;
+        tripUpdateCache.values().removeIf(feedEntity -> {
+            long age = nowSecs - findLatestTimestamp(feedEntity);
+            if (age > tripUpdateMaxAgeSecs) {
+                log.debug("Removing because age is {} s", age);
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+        List<GtfsRealtime.FeedEntity> feedEntities = new ArrayList<>();
+        feedEntities.addAll(tripUpdateCache.values());
+        if (latestServiceAlerts != null) {
+            feedEntities.addAll(latestServiceAlerts);
+        }
+
+        GtfsRealtime.FeedMessage feedMessage = FeedMessageFactory.createFullFeedMessage(feedEntities, nowSecs);
+
+        log.info("Publishing a new Service Alert");
+        byte[] data = feedMessage.toByteArray();
+        sink.put(fileName, data);
     }
 
+    static boolean isCancellation(GtfsRealtime.TripUpdate tripUpdate) {
+        return tripUpdate.getTrip().hasScheduleRelationship() && tripUpdate.getTrip().getScheduleRelationship() == GtfsRealtime.TripDescriptor.ScheduleRelationship.SCHEDULED ||
+                tripUpdate.getStopTimeUpdateList().stream().anyMatch(stopTimeUpdate -> stopTimeUpdate.hasScheduleRelationship() && stopTimeUpdate.getScheduleRelationship() == GtfsRealtime.TripUpdate.StopTimeUpdate.ScheduleRelationship.SKIPPED);
+    }
+
+    static long findLatestTimestamp(GtfsRealtime.FeedEntity entity) {
+        Optional<Long> maxTimestamp = entity.getTripUpdate().getStopTimeUpdateList().stream()
+                .map(stu -> {
+                    long max = 0L;
+                    if (stu.hasArrival()) {
+                        max = stu.getArrival().getTime();
+                    }
+                    if (stu.hasDeparture()) {
+                        long departureTime = stu.getDeparture().getTime();
+                        if (departureTime > max) {
+                            max = departureTime;
+                        }
+                    }
+                    return max;
+                }).max(Comparator.naturalOrder()); // Get latest timestamp of all StopTimeUpdates in this TripUpdate
+        return maxTimestamp.orElse(0L);
+    }
 
 }
